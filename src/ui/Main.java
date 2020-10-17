0da2af5e286fa9ef77e07a46f161b2383e0f3c95
AngelicaCorrales <angelicacorrales2002@gmail.com>
2020-10-16 20:59:22 -0500

    Modifique el metodo registerPilots y createChampionship del main

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -18,18 +18,46 @@ public class Main{
 
 	public void menu(){
 		boolean menu=true;
-
 	}
 
 	public void createChampionship(){
 		System.out.println("-----------------------------------------------------------");
-		System.out.pritnln("CAMPEONATO");
+		System.out.pritnln("CAMPEONATO FIA \n");
 		System.out.pritnln("Ingrese el anio del campeonato");
 		int year= sc.nextInt();
-		System.out.pritnln("Ingrese el numero de carreras del campeonato");
+		System.out.pritnln("\nIngrese el numero de carreras del campeonato");
 		int races= sc.nextInt();
 
-		championship= new Championship();
+		championship= new Championship(year, races);
 	}
+
+	public void registerPilots(){
+		sc.nextLine();
+		System.out.println("-----------------------------------------------------------");
+		System.out.pritnln("REGISTRAR PILOTO \n");
+		//	public Pilot(String pName, int pAge, Team pTeam, int[] pScores)
+		System.out.pritnln("Ingrese el nombre del piloto");
+		String name=sc.nextLine();
+		System.out.pritnln("\nIngrese la edad del piloto");
+		int age=sc.nextInt();
+		sc.nextLine();
+		System.out.pritnln("\nIngrese el equipo al que pertenece el piloto");
+		String team=sc.nextLine();
+		///HAY QUE CONVERTIR EL STRING EN TEAM!!!
+		int[] scoreRaces= new int [championship.getRaces()];
+		for(int i=0; i<scoreRaces.length; i++){
+			System.out.pritnln("Ingrese el tiempo en segundos del piloto en la carrera "+(i+1));
+			scoreRaces[i]=sc.nextInt();
+		}
+		championship.addPilot(name, age, team, scoreRaces);
+		
+
+		System.out.println("-----------------------------------------------------------");
+		if(championship.findPilot(name)){
+			System.out.pritnln("No es posible registrar el piloto, ya ha sido registrado anteriormente");
+		}
+	}
+
+
 
 }
